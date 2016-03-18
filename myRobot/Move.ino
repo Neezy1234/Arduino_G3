785b51e5486123b70fa90303988d0f0ceafbc9ee
fankohr <valentin.courdy@gmail.com>
2016-03-18 15:25:17 +0100

    Modification du fichier principal & ajout du fichier Move qui gère les déplacements

---
+++ myRobot/Move.ino
@@ -0,0 +1,36 @@
+void Move(char input) {
+//Move forward, backward, right and left depending on the input.
+//Valid input:
+// z(forward)
+// s(backward)
+// q(left)
+// d(right)
+
+  switch(input){
+      case 'z' :
+        // Move forward;
+        servo1.write(0);
+        servo2.write(180);
+        break;
+      case 's':
+        // Move backward;
+        servo1.write(180);
+        servo2.write(0);
+        break;
+      case 'q' :
+        // Move left;
+        servo1.write(0);
+        servo2.write(0);
+        break;
+      case 'd':
+        // Move right;
+        servo1.write(180);
+        servo2.write(180);
+        break;
+      case 'p':
+        // Pause/Stop both servos
+        servo1.write(90);
+        servo2.write(90);
+        break;
+    }
+}
