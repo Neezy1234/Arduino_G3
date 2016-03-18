172afeedc988a48c91425b11a39cfa0b3e6599d4
Jawstin <justin.fayard@gmail.com>
2016-03-18 21:20:27 +0100

    Modification du fichier myRobot.ino dans le but de clarifier le commit/push precedent. + Re arrangement du code afin que celui-ci reste coherent, + Ajout de commentaires pour expliquer plus en details ce que fait le programme.

--- myRobot/myRobot.ino
+++ myRobot/myRobot.ino
@@ -1,31 +1,52 @@
-#include <ArduinoUnit.h>
+/*
+ * Projet Agilite/Application embarque
+ * IUT Informatique - Aix-en-Provence
+ * 
+ * Groupe 3 :
+ * Yasmine AMAR, Valentin COURDY,
+ * Sandro ENCISO, Justin FAYARD
+ * Hafid HOUT, Alexis MARTIN
+ * 
+ */
 
+
+#include <ArduinoUnit.h>
 #include <Servo.h>
 #include <SPI.h>
 
+//Declaration des servos 1 et 2
 Servo servo1;
 Servo servo2;
 
-int buttonState = 0;     
+//Declaration des variables globales (etats, valeurs...)
+int buttonState = 0;   
+int frontSensor = 0;
+int backSensor = 0;
+int leftSensor = 0;
+int rightSensor = 0;
+
 boolean power = false;          
 boolean obstacle = false;
 
-const int buttonPin = PIN_SW0;    // the number of the pushbutton pin
-const int ledPin    = PIN_LED_13; // the number of the LED pin
+
+//Declaration des PINs
+const int buttonPin = PIN_SW0;    // Numero du pin du bouton
+const int ledPin    = PIN_LED_13; // Numero du pin de la LED
+const int frontSensorPin = A0;    // Numero du pin du capteur avant
+const int backSensorPin  = A1;    // Numero du pin du capteur arriere
+const int leftSensorPin  = A2;    // Numero du pin du capteur gauche
+const int rightSensorPin = A3;    // Numero du pin du capteur droit
 
 void setup() {
   // put your setup code here, to run once:
 
   pinMode(buttonPin, INPUT_PULLUP);
 
-  //pinMode(A0, OUTPUT);
-  //digitalWrite(A0, HIGH);
-
   //"Initialise" les servo 1 et 2
   servo1.attach(16);
   servo2.attach(17);
 
-  //Position des servos à l'arrêt/par défaut
+  //Position des servos à l'arret/par défaut
   servo1.write(90);
   servo2.write(90);
 
@@ -38,35 +59,58 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly
 
+  //On lit l'etat du bouton (appuye ou pas
   buttonState = digitalRead(buttonPin);
 
-  analogReadResolution(8);
-  Serial.print(", 8-bit : ");
-  Serial.println(analogRead(A0));
-  
+  //Si le bouton est appuye on change le boolean "power"
   if(buttonState == LOW) {
     power = !power;
-    if(power == false) Move('p');
+    if(power == false) Move('p'); //Pause des servos
     delay(500);
   }
 
+  //Si "power" est true on effectue le code sinon on fait rien
   if(power == true) {
-    if(analogRead(A0) > 120 ) {
+
+    //Change la resolution de la lecture analogue (8 bits)
+    analogReadResolution(8);
+
+    //On lit les valeurs des capteurs
+    frontSensor = analogRead(frontSensorPin);
+    /*
+    backSensor  = analogRead(backSensorPin);
+    leftSensor  = analogRead(leftSensorPin);
+    rightSensor = analogRead(rightSensorPin);
+    */
+    
+    //On affiche les valeurs des capteurs
+    Serial.print("Front sensor : ");
+    Serial.println(frontSensor);
+    /*
+    Serial.print("Back sensor : ");
+    Serial.println(backSensor);
+    Serial.print("Left sensor : ");
+    Serial.println(leftSensor);
+    Serial.print("Right sensor : ");
+    Serial.println(rightSensor);
+    */
+
+    //On indique si un obstacle est present devant
+    if(frontSensor > 120 ) {
       obstacle = true;
     }
     else {
       obstacle = false;
     }
-    
+
+    //Si il y a un obstacle on s'arrete sinon on avance
     if(obstacle == false) {
-      Move('z');  
+      Move('z'); //Avance
     }
     else {
-      Move('p');
+      Move('p'); //Pause
     }
   }
-  else {}
 
-  
 }
 
