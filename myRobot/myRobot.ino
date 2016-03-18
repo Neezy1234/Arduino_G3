4988525c37a0d193af95eaea243ff18b8eddb505
Jawstin <justin.fayard@gmail.com>
2016-03-18 18:06:21 +0100

    Mise en place du bouton d'arret + Arret si obstacle proche

--- myRobot/myRobot.ino
+++ myRobot/myRobot.ino
@@ -1,43 +1,3 @@
-#include <BlynkApiArduino.h>
-#include <BlynkApiParticle.h>
-#include <BlynkParticle.h>
-#include <BlynkSimpleCC3000.h>
-#include <BlynkSimpleEnergiaWiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <BlynkSimpleEsp8266_SSL.h>
-#include <BlynkSimpleEthernet.h>
-#include <BlynkSimpleEthernet2.h>
-#include <BlynkSimpleEthernetV2_0.h>
-#include <BlynkSimpleIntelEdisonWiFi.h>
-#include <BlynkSimpleLinkItONE.h>
-#include <BlynkSimpleMKR1000.h>
-#include <BlynkSimpleParticle.h>
-#include <BlynkSimpleRBL_CC3200.h>
-#include <BlynkSimpleRBL_WiFi_Mini.h>
-#include <BlynkSimpleRedBear_Duo.h>
-#include <BlynkSimpleSerial.h>
-#include <BlynkSimpleShieldEsp8266.h>
-#include <BlynkSimpleShieldEsp8266_HardSer.h>
-#include <BlynkSimpleShieldEsp8266_SoftSer.h>
-#include <BlynkSimpleTinyDuino.h>
-#include <BlynkSimpleTI_CC3200_LaunchXL.h>
-#include <BlynkSimpleTI_TivaC_Connected.h>
-#include <BlynkSimpleUIPEthernet.h>
-#include <BlynkSimpleUserDefined.h>
-#include <BlynkSimpleWifi.h>
-#include <BlynkSimpleWiFiShield101.h>
-#include <BlynkSimpleWiFiShield101_SSL.h>
-#include <BlynkSimpleWiFly.h>
-#include <BlynkSimpleWildFire.h>
-#include <BlynkSimpleYun.h>
-#include <BlynkWidgets.h>
-#include <WidgetBridge.h>
-#include <WidgetLCD.h>
-#include <WidgetLED.h>
-#include <WidgetRTC.h>
-#include <WidgetSD.h>
-#include <WidgetTerminal.h>
-
 #include <ArduinoUnit.h>
 
 #include <Servo.h>
@@ -45,14 +5,21 @@
 
 Servo servo1;
 Servo servo2;
-const int buttonPin = PIN_SW0;
-int buttonState = 0;              
-char input;                       // input du port Serial afin de controller les Servo
+
+int buttonState = 0;     
+boolean power = false;          
+boolean obstacle = false;
+
+const int buttonPin = PIN_SW0;    // the number of the pushbutton pin
+const int ledPin    = PIN_LED_13; // the number of the LED pin
 
 void setup() {
   // put your setup code here, to run once:
 
   pinMode(buttonPin, INPUT_PULLUP);
+
+  //pinMode(A0, OUTPUT);
+  //digitalWrite(A0, HIGH);
 
   //"Initialise" les servo 1 et 2
   servo1.attach(16);
@@ -70,6 +37,36 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly
-  Move('z');  
+
+  buttonState = digitalRead(buttonPin);
+
+  analogReadResolution(8);
+  Serial.print(", 8-bit : ");
+  Serial.println(analogRead(A0));
+  
+  if(buttonState == LOW) {
+    power = !power;
+    if(power == false) Move('p');
+    delay(500);
+  }
+
+  if(power == true) {
+    if(analogRead(A0) > 120 ) {
+      obstacle = true;
+    }
+    else {
+      obstacle = false;
+    }
+    
+    if(obstacle == false) {
+      Move('z');  
+    }
+    else {
+      Move('p');
+    }
+  }
+  else {}
+
+  
 }
 
